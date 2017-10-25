b76e43bba80a6bf9beb6dd9cfc5375ba4cbfcfb5
SwallowJoe <972605045@qq.com>
2017-10-25 23:11:19 +0800

    SwallowJoe first commit

--- src/com/game/Character/Level.java
+++ src/com/game/Character/Level.java
@@ -5,8 +5,6 @@ package com.game.Character;
  * at 2017/10/23
  */
 
-import com.game.Common.GlobalConstance;
-
 /**
  * 所有的物品以及人物公用的等级机制
  */
@@ -85,5 +83,30 @@ public class Level {
                 ", name='" + name + '\'' +
                 ", expPool=" + expPool +
                 '}';
+    }
+    /**
+     * 全局常量
+     */
+    static class GlobalConstance {
+        /*低等级和高等级对应的经验池的大小*/
+        public static final int LOW_HIGH_DIVIDE=4;
+        public static final double LOW_LEVEL_EXP_POOL=200;
+        public static final double HIGH_LEVEL_EXP_POOL=20000;
+        /*等级及其对应的评价*/
+        public static final String[] RANK={
+                "斗之气"
+                ,"斗者"
+                ,"斗师"
+                ,"大斗师"
+                ,"斗灵"
+                ,"斗王"
+                ,"斗皇"
+                ,"斗宗"
+                ,"斗尊"
+                ,"斗尊巅峰"
+                ,"半步斗圣"
+                ,"斗圣"
+                ,"斗帝"
+        };
     }
 }
